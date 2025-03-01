03062cb0eb37e300fc67fa40c4f185dacdc47204
Shubham Kumar Gupta <gptshubham595@gmail.com>
2025-03-01 05:45:40 +0530

    HILT API Constant

--- android/core/src/main/java/com/insta/core/repository/VideoRepositoryImpl.kt
+++ android/core/src/main/java/com/insta/core/repository/VideoRepositoryImpl.kt
@@ -10,46 +10,49 @@ import com.insta.domain.models.VideoResponse
 import com.insta.domain.repository.VideoRepository
 import javax.inject.Inject
 
-const val BASE_URL = "http://192.168.29.179:3000"
 
-class VideoRepositoryImpl @Inject constructor(private val apiService: ApiService) :
+class VideoRepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val localIpAddress: String
+) :
     VideoRepository {
+
     override suspend fun getFeed(page: Int): List<VideoResponse> {
-        return apiService.getFeed(page).videos.map { it.toVideoResponse() }
+        return apiService.getFeed(page).videos.map { it.toVideoResponse(localIpAddress) }
     }
 
     override suspend fun getVideo(videoId: String): VideoResponse {
-        return apiService.getVideo(videoId).toVideoResponse()
+        return apiService.getVideo(videoId).toVideoResponse(localIpAddress)
     }
 }
 
-private fun VideoResponseDTO.toVideoResponse(): VideoResponse {
+private fun VideoResponseDTO.toVideoResponse(localIpAddress: String): VideoResponse {
     return VideoResponse(
         id = id,
         title = title,
         description = description,
         duration = duration,
-        dashManifest = BASE_URL + dashManifest,
-        thumbnail = BASE_URL + thumbnail,
-        firstChunk = firstChunk?.toChunkResponse(),
-        chunks = chunks?.map { it.toChunkResponse() }
+        dashManifest = localIpAddress + dashManifest,
+        thumbnail = localIpAddress + thumbnail,
+        firstChunk = firstChunk?.toChunkResponse(localIpAddress),
+        chunks = chunks?.map { it.toChunkResponse(localIpAddress) }
     )
 }
 
-private fun ChunkDTO.toChunkResponse(): Chunk {
+private fun ChunkDTO.toChunkResponse(localIpAddress: String): Chunk {
     return Chunk(
         index = index,
         startTime = startTime,
         duration = duration,
-        urls = urls?.toUrlResponse()
+        urls = urls?.toUrlResponse(localIpAddress)
     )
 }
 
-private fun UrlsDTO.toUrlResponse(): Urls {
+private fun UrlsDTO.toUrlResponse(localIpAddress: String): Urls {
     return Urls(
-        high = BASE_URL + high,
-        medium = BASE_URL + medium,
-        low = BASE_URL + low
+        high = localIpAddress + high,
+        medium = localIpAddress + medium,
+        low = localIpAddress + low
     )
 }
 
